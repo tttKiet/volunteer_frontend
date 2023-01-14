97cc4231d98a33fd0a236465ab98c14527d64eb7
BuiKiet <roiyano1utl@gmail.com>
2023-01-14 15:12:33 +0700

    8. Save state with persistReducer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 import GlobalStyles from '~/components/GlobalStyles';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,7 +14,9 @@ root.render(
     <React.StrictMode>
         <GlobalStyles>
             <Provider store={store}>
-                <App />
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
             </Provider>
         </GlobalStyles>
     </React.StrictMode>,
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,23 @@
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from '../reducers';
 
+const userPersistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(userPersistConfig, userSlice.reducer);
+
 const store = configureStore({
     reducer: {
-        user: userSlice.reducer,
+        user: persistedReducer,
     },
 });
+
+const persistor = persistStore(store);
+export { persistor };
+
 export default store;
