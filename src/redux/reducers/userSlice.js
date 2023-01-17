13f6fc0ab9c7c4d2d8b51520f6d3bece8614ba51
BuiKiet <roiyano1utl@gmail.com>
2023-01-17 23:03:59 +0700

    7. edit db, add fr key

--- src/redux/reducers/userSlice.js
+++ src/redux/reducers/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        isLogined: true,
+        isLogined: false,
         userData: {},
     },
     reducers: {
